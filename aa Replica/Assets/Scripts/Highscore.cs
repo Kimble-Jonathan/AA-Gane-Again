76fe061e8654fdfd0bb4b5352534ff9e2c519ba9
Kimble-Jonathan <54344712+Kimble-Jonathan@users.noreply.github.com>
2019-10-10 21:04:39 -0500

    AA-Game

---
+++ aa Replica/Assets/Scripts/Highscore.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+    public static int number;
+    public Text score;
+    public Text highScore;
+    public static string PlayerName;
+    public InputField PlayernameInput;
+    public Text Name;
+    public Text LastScore;
+    public Text Rounds;
+
+
+
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt(PlayerName, 0).ToString();
+
+        LastScore.text = PlayerPrefs.GetInt("score", number).ToString();
+
+    }
+
+
+    public void Update()
+    {
+        Name.text = PlayerName;
+
+        number = Score.PinCount;
+        score.text = number.ToString();
+        PlayerPrefs.SetInt("score", number);
+
+        if (number > PlayerPrefs.GetInt(PlayerName, 0))
+        {
+            PlayerPrefs.SetInt(PlayerName, number);
+            highScore.text = number.ToString();
+        }
+
+    }
+    public void Reset()
+    {
+        PlayerPrefs.DeleteKey(PlayerName);
+        highScore.text = "0";
+    }
+
+    public void UpdateName()
+    {
+
+        PlayerName = PlayernameInput.text;
+        PlayerPrefs.SetString(PlayerName.ToString(), PlayerName);
+
+
+    }
+}
+
+
+
---
+++ aa Replica/Assets/Scripts/Scenes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scenes : MonoBehaviour
+{
+
+    public float Rounds=0;
+    public Text RoundsText;
+   public void StartPlay()
+    {
+        Rounds = PlayerPrefs.GetFloat("Round", Rounds);
+        Rounds +=  1;
+        PlayerPrefs.SetFloat("Round", Rounds);
+        
+        SceneManager.LoadScene("MainLevel");
+        
+    }
+
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void ExitGame()
+    {
+ 
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    public void Update()
+    {
+        
+        RoundsText.text = PlayerPrefs.GetFloat("Round", 0).ToString();
+    }
+}
